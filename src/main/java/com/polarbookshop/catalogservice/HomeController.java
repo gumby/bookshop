18b5c5152bd82f947f4c671d31a1e71ddfdd3809
Patrick Gombola <pgombola@burrislogistics.com>
2023-04-13 15:32:51 -0400

    Add greeting property

--- src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -1,13 +1,20 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.config.PolarProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    private final PolarProperties props;
+
+    public HomeController(PolarProperties props) {
+        this.props = props;
+    }
+
     @GetMapping("/")
     public String getGreeting() {
-        return "Welcome to the book catalog!";
+        return props.getGreeting();
     }
 }
