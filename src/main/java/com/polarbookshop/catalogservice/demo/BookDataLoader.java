4ea4e620e5e9e09ca8761f4a431cc1d7dfbaac62
Patrick Gombola <pgombola@burrislogistics.com>
2023-04-15 11:39:21 -0400

    Add spring data repo

--- src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
+++ src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @Profile("testdata")
 public class BookDataLoader {
@@ -19,9 +21,9 @@ public class BookDataLoader {
 
     @EventListener(ApplicationReadyEvent.class)
     public void loadBookTestData() {
+        bookRepo.deleteAll();
         var book1 = Book.of("1234567891", "Northern Lights", "Lyra Silverstart", 9.90);
         var book2 = Book.of("1234567892", "Polar Journey", "Iorek Polarson", 12.90);
-        bookRepo.save(book1);
-        bookRepo.save(book2);
+        bookRepo.saveAll(List.of(book1, book2));
     }
 }
